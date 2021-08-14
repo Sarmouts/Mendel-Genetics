868c502cf4aa1b112d66d59c25051c707c6aae2d
Sarmouts <saradis.m06@gmail.com>
2021-08-14 15:53:50 +0300

    ncwo8

--- Mendel/Mendel.cpp
+++ Mendel/Mendel.cpp
@@ -25,16 +25,58 @@ int main()
 		cin >> M2;
 		if (F1 == 'A' || F1 == 'a' && F2 == 'A' || F2 == 'a' && M1 == 'A' || M1 == 'a' && M2 == 'A' || M2 == 'a')
 		{
+			cout << "==================================================================================" << endl;
 			cout << "Father's genotype: " << F1 << F2 << "      X      " << " Mother's genotype: " << M1 << M2 << endl;
 			cout << "Father's gametes: " << F1 << "," << F2 << "," << F1 << "," << F2 << "          Mother's gametes: " << M1 << "," << M2 << "," << M1 << "," << M2 << endl;
 			cout << endl;
 			cout << " Child's possible genotype : " << endl;
 			cout << endl;
 			cout << "M/F| " << F1 << "  | " << F2 << "  | " << endl;
+			cout << "--------------" << endl;
 			cout << M1 << "  | " << F1 << M1 << " | " << F2 << M1 << " | " << endl;
+			cout << "--------------" << endl;
 			cout << M2 << "  | " << F1 << M2 << " | " << F2 << M2 << " | " << endl;
-			cout << "==================================================================================" << endl;
+			cout << "------------------------------------------------------------------------------------------" << endl;
 			//*possibilites of a phenotype and a genotype still in progress...
+			if (F1 == M1 == F2 == M2)
+			{
+				if (F1 == 'A')
+				{
+					cout << "100% for genotype AA and 100% for phenotype A" << endl;
+					cout << "==================================================================================" << endl;
+					//100% AA
+				}
+				else if (F1 == 'a')
+				{
+					cout << "100% for genotype aa and 100% for phenotype a" << endl;
+					cout << "==================================================================================" << endl;
+					//100% aa
+				}
+			}
+			else if (F1 == M1 && F2 == M2)
+			{
+				cout << "50% for genotype Aa, 25% for genotype AA, 25% for genotype aa and 75% for phenotype A, 25% for phenotype a" << endl;
+				cout << "=============================================================================================================" << endl;
+				//50% for Aa, 25% for AA, 25% for aa
+			}
+			else if (F1 == M1 && F2 != M2)
+			{
+				cout << "50% for genotype AA, 50% for genotype Aa and 100% for phenotype A" << endl;
+				cout << "==================================================================================" << endl;
+				//50%AA 50%Aa
+			}
+			else if (F1 != M1 && F2 == M2)
+			{
+				cout << "50% for genotype aa, 50% for genotype Aa and 50% for phenotype a, 50% for phenotype A" << endl;
+				cout << "========================================================================================" << endl;
+				//50%aa 50%Aa
+			}
+			else if (F1 != M1 && F2 != M2)
+			{
+				cout << "100% for genotype Aa and 100% for phenotype A" << endl;
+				cout << "==================================================================================" << endl;
+				//100% Aa
+			}
 		}
 		else
 		{
